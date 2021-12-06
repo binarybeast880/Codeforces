d86334eb931a4124b0f39cdf3d9d231fe89f328e
Anurag Roy <89125578+royanurag880@users.noreply.github.com>
2021-12-06 17:20:20 +0530

    Add files via upload

--- 1481a.cpp
+++ 1481a.cpp
@@ -1,34 +1,45 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-main(){
-int t,x,y;
-string s;
-cin>>t;
-while(t--){
-    cin>>x>>y;
-    cin>>s;
-    int u=0,d=0,r=0,l=0;
-    for(int i=0;i<s.length();i++){
-        if(s[i]=='U')
-            u++;
-        else if(s[i]=='D')
-            d++;
-        else if(s[i]=='L')
-            l++;
+#define vi vector<int>
+#define ll long long
+#define vs vector<string>
+main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        string s;
+        cin >> s;
+        bool found = false;
+        int u = 0, d = 0, l = 0, r = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] == 'U')
+                u++;
+            else if (s[i] == 'D')
+                d++;
+            if (s[i] == 'R')
+                r++;
+            else if (s[i] == 'L')
+                l++;
+        }
+        if (x > 0 && r >= x)
+            x = 0;
+        if (x < 0 && l >= -x)
+            x = 0;
+        if (y > 0 && u >= y)
+            y = 0;
+        if (y < 0 && d >= -y)
+            y = 0;
+        if (x == 0 && y == 0)
+            cout << "YES"
+                 << "\n";
         else
-            r++;
+            cout << "NO"
+                 << "\n";
+        // cout << ((!x && !y) ? "YES" : "NO") << endl;
     }
-    if(x>0 && r>=x)
-        x=0;
-    else if(x<0 && l>=-x)
-        x=0;
-    else if(y>0 && u>=y)
-        y=0;
-    else if(y<0 && d>=-y)
-        y=0;
-    if(x==0 && y!=0)
-        cout<<"YES"<<endl;
-    else
-        cout<<"NO"<<endl;
-}
 }
