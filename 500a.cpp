7df0c0fb21db6d60b621f905d6e5402f44ba2c84
Anurag Roy <89125578+royanurag880@users.noreply.github.com>
2021-12-07 22:36:27 +0530

    Add files via upload

--- 500a.cpp
+++ 500a.cpp
@@ -1,21 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-main(){
-	int n,d;
-	cin>>n>>d;
-	int a[n-1],sum=0;
-	for(int i=0;i<n-1;i++){
-		cin>>a[i];
-	}
-	for(int i=1;i<n-1;i++){
-		sum+=a[i];
-		if(sum==d){
-            cout<<"YES"<<endl;
-            return 0;
-		}
-		  i = a[i-1]+i;
-        if(i > n)
-            break;
-	}
-		cout<<"NO"<<endl;
+int a[1234567];
+main()
+{
+    int n, t;
+    cin >> n >> t;
+    for (int i = 1; i <= n - 1; i++)
+        cin >> a[i];
+    int x = 1;
+    while (x < t)
+    {
+        x += a[x];
+    }
+    if (x == t)
+        cout << "YES";
+    else
+        cout << "NO";
 }
